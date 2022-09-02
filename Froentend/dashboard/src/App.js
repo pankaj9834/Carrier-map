a1cc0a4696ab34a01379a084999ae69d94212a4e
Ashish Srivastav <ashishshrivastav275@gmail.com>
2022-09-03 00:44:09 +0530

    changes2

--- Froentend/dashboard/src/App.js
+++ Froentend/dashboard/src/App.js
@@ -1,26 +1,38 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Dash from "./Components/Dash";
-
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import GovernmentJobPage from "./Components/GovernmentJobPage";
+import { auth } from "./firebase";
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
   return (
-    /*<Container>
+    <div className="App">
       <Router>
-        <Sidebar />
-        <MainContent />
-        </Container>*/
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Dash />} />
-        <Route
-          exact
-          path="/GovernmentJobPage"
-          element={<GovernmentJobPage />}
-        />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route exact path="/" element={<Dash />} />
+          <Route
+            exact
+            path="/GovernmentJobPage"
+            element={<GovernmentJobPage />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Dash name={userName} />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- Froentend/dashboard/src/Components/GovernmentJobPage.js
+++ Froentend/dashboard/src/Components/GovernmentJobPage.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function GovernmentJobPage() {
   return (
-    <div>GovernmentJobPage
-    <h1>Welcome to  my world</h1></div>
-  )
+    <div>
+      GovernmentJobPage
+      <h1>welomet TO GovernmentJobsFairs</h1>
+    </div>
+  );
 }
 
-export default GovernmentJobPage
+export default GovernmentJobPage;
